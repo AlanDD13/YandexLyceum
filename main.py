2a5a116cb9d54ffccc2e603af1b616611dba0082
AlanDD13 <alanduis@yandex.ru>
2021-11-23 17:13:34 +0300

    asds

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import sys
-from random import randint
+from random import randint, choice
 
 from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
+from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import QApplication, QWidget
 
 
 class Example(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.qp = QPainter()
         self.flag = False
+        self.colors = ['Red', 'Orange', 'Yellow', 'Green', 'Cyan',
+                       'Blue', 'Magenta', 'Purple', 'Brown', 'Black']
         self.pushButton.clicked.connect(self.drawf)
 
     def drawf(self):
@@ -27,11 +30,26 @@ class Example(QWidget):
 
     def draw(self):
         R = randint(20, 100)
-        x = randint(0, 400)
-        y = randint(0, 400)
-        self.qp.setBrush(QColor('Yellow'))
+        x = randint(0, 250)
+        y = randint(0, 250)
+        self.qp.setBrush(QColor(choice(self.colors)))
         self.qp.drawEllipse(x, y, R, R)
-        self.update()
+
+
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 300)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(120, 210, 111, 41))
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "PushButton"))
 
 
 if __name__ == '__main__':
